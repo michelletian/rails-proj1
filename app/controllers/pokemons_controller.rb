272a00be923fa936737c99c7da438b53fe75caca
michellectian@gmail.com <michellectian@gmail.com>
2014-11-02 00:46:01 -0700

    I am a Pokemon master

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -7,15 +7,17 @@ class PokemonsController < ApplicationController
 	end
 
 	def damage
-		puts 7
 		@pokemon = Pokemon.find(params[:id])
 		@pokemon.health -= 10
 		@pokemon.save
-		if @pokemon.health <= 0
-			@pokemon.destroy
-		end
-		puts 5
-		redirect_to trainers_path
+		redirect_to trainer_path(@pokemon.trainer_id)
+	end
+
+	def heal
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.health += 10
+		@pokemon.save
+		redirect_to trainer_path(@pokemon.trainer_id)
 	end
 
 	def new
@@ -23,7 +25,21 @@ class PokemonsController < ApplicationController
 	end
 
 	def create
-
+		@pokemon = Pokemon.new(pokemon_params)
+		if @pokemon.valid?
+			@pokemon.trainer_id = current_trainer.id
+			@pokemon.health = 100
+			@pokemon.level = 1
+			@pokemon.save
+			redirect_to trainer_path(current_trainer.id)
+		else 
+			flash[:error] = @pokemon.errors.full_messages.to_sentence
+			redirect_to new_pokemon_path
+		end
 	end
+
+	def pokemon_params
+    params.require(:pokemon).permit(:name)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  patch '/damage_path', to: 'pokemons#damage', as: :damage
+  patch '/heal_path', to: 'pokemons#heal', as: :heal
   devise_for :trainers
   resources :trainers
   patch '/home/capture_path', to: 'pokemons#capture', as: :capture
-  patch '/trainers/damage_path', to: 'pokemons#damage', as: :damage
-  get '/pokemons/new', to: 'pokemons#create', as: :pokemons
+  resources :pokemons
 end
